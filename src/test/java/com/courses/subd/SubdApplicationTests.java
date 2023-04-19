d66df122d13fd115f44e716edc43c8ab72ba88dc
A2sh3r <ashurmetovmm@gmail.com>
2023-04-19 22:33:23 +0300

    first commit

---
+++ src/test/java/com/courses/subd/SubdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.courses.subd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
